e453aa48842b517e9d2c0cf98e2b857825ef9c61
lastpagevlad <116770372+lastpagevlad@users.noreply.github.com>
2023-09-22 00:04:36 +0300

    #10 add dto and controller valid (#14)
    * #7 добавление исключения обновлению сообщения
    
    * #7 добавление исключения обновлению сообщения
    
    * #11 добавление классов DTO и MappingUtils

---
+++ src/main/java/org/example/dto/UserDto.java
@@ -0,0 +1,11 @@
+package org.example.dto;
+
+import lombok.Data;
+@Data
+public class UserDto {
+    Integer id;
+    String name;
+    String surname;
+    String lastname;
+    String email;
+}
--- src/main/java/org/example/service/MessageServiceImpl.java
+++ src/main/java/org/example/service/MessageServiceImpl.java
@@ -1,12 +1,18 @@
 package org.example.service;
 
+import jakarta.validation.ValidationException;
 import org.example.store.entity.Message;
 import org.example.store.repository.MessageRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.store.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -19,10 +25,11 @@ public class MessageServiceImpl implements MessageService {
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
 
+
     @Override
     public Message create(Message message) {
-        message.setUser(userRepository.findById(message.getUser().getId()).get());
-        return messageRepository.save(message);
+            message.setUser(userRepository.findById(message.getUser().getId()).get());
+            return messageRepository.save(message);
     }
 
     @Override
@@ -32,7 +39,8 @@ public class MessageServiceImpl implements MessageService {
 
     @Override
     public Message read(int id) {
-        return messageRepository.findById(id).orElse(null);
+        return messageRepository.findById(id).orElseThrow(() ->
+                new NoSuchElementException(String.format("message with id: %s not found", id)));
     }
 
     @Override
@@ -49,13 +57,8 @@ public class MessageServiceImpl implements MessageService {
                     new NoSuchElementException(String.format("user with id: %s not found", idUser)))
             );
         }
-
-
-
         updateMessage.setMessageSend(message.getMessageSend());
         updateMessage.setDateSending(message.getDateSending());
-
-
         return messageRepository.save(updateMessage);
     }
 
